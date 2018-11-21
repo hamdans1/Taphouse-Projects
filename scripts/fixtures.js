7c992720f4c5be16084fffd51d1c5d3c3d438852
Sami Hamdan <samihamdan@Samis-iMac.local>
2018-11-21 16:06:50 -0500

    started beer fixtures

---
+++ scripts/fixtures.js
@@ -0,0 +1,104 @@
+const beer = [
+  {
+    name: 'Guinness',
+    type: 'Irish Dry Stout',
+    abv: 4.2,
+    price: 10,
+    brewery: 'Guinness Brewing',
+    location: 'Dublin, Ireland',
+    description: ''
+  },
+  {
+    name: 'Radeberger',
+    type: 'German Pilsner',
+    abv: 4.8,
+    price: 9,
+    brewery: 'Radeberger Gruppe',
+    location: 'Radeberg, Germany',
+    description: ''
+  },
+  {
+    name: 'Sam Seasonal - Winter Lager',
+    type: 'Maibock',
+    abv: 5.6,
+    price: 9,
+    brewery: 'Sam Adams',
+    location: 'Boston, MA',
+    description: ''
+  },
+  {
+    name: 'Modelo Especial',
+    type: 'Mexican Lager',
+    abv: 4.4,
+    price: 9,
+    brewery: 'Modelo',
+    location: 'Mexico City, Mexico',
+    description: ''
+  },
+  {
+    name: 'Veltins Pilsner',
+    type: 'German Pilsner',
+    abv: 4.8,
+    price: 8,
+    brewery: 'Veltins',
+    location: 'Meschede-Grevenstein, Germany',
+    description: ''
+  },
+  {
+    name: 'Peak Organic IPA',
+    type: 'India Pale Ale',
+    abv: 7.2,
+    price: 9,
+    brewery: 'Peak Organic Brewery',
+    location: 'Portland, ME',
+    description: ''
+  },
+  {
+    name: '60 Minute IPA',
+    type: 'India Pale Ale',
+    abv: 6.0,
+    price: 9,
+    brewery: 'Dogfish Head',
+    location: 'Milton, DE',
+    description: ''
+  },
+  {
+    name: 'Brooklyn Lager',
+    type: 'Amber Lager',
+    abv: 5.2,
+    price: 9,
+    brewery: 'Brooklyn Brewery',
+    location: 'Brooklyn, NY',
+    description: ''
+  },
+  {
+    name: 'Hoppy Lager',
+    type: 'India Pale Lager',
+    abv: 6.0,
+    price: 9,
+    brewery: 'Five Boroughs',
+    location: 'Brooklyn, NY',
+    description: ''
+  },
+  {
+    name: 'Switchback Ale',
+    type: 'American Red Ale',
+    abv: 5.0,
+    price: 9,
+    brewery: 'Switchback Brewing Co.',
+    location: 'Burlington, VT',
+    description: ''
+  },
+  {
+    name: 'Blue Moon Belgian White',
+    type: 'Belgian Witbier',
+    abv: 5.4,
+    price: 9,
+    brewery: 'Coors Brewing Co.',
+    location: 'Golden, CO',
+    description: ''
+  },
+  {
+    
+  }
+];
